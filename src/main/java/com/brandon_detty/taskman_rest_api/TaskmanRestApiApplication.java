cedbe6e2ce926d9ba8d3dd0198d59f357b59df02
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2022-12-06 11:10:03 -0800

    Spring Init

---
+++ src/main/java/com/brandon_detty/taskman_rest_api/TaskmanRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.brandon_detty.taskman_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanRestApiApplication.class, args);
+	}
+
+}
